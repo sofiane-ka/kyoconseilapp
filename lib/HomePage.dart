1247c282b9a315c352af86988e5b1b71a001eef7
Sofiane KARAOUNI <116922781+sofiane-ka@users.noreply.github.com>
2023-03-10 13:19:16 +0100

    main page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -6,6 +6,7 @@ import 'ChatMessageWidget.dart';
 const backgroundColor = Color(0xffffffff);
 const iconColor = Color(0xff09080);
 const redColor = Color(0xffFF2D56);
+const grayColor = Color(0xffF3F3F3);
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,62 +21,141 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 7.0),
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            centerTitle: true,
-            elevation: 0.0,
-            leading: Icon(
-              Icons.menu,
-              color: Colors.black,
-            ),
-            title: Image.asset(
-              'assets/note.png',
-            ),
-            actions: [
-              Icon(
-                Icons.person,
-                color: Colors.black,
-              )
-            ],
-          ),
-          extendBodyBehindAppBar: true,
-          backgroundColor: backgroundColor,
-          body: SafeArea(
-            child: Center(
-                child: Column(children: [
-              SizedBox(
-                height: 40.0,
+    return Container(
+        color: Colors.white,
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 7.0),
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                centerTitle: true,
+                elevation: 0.0,
+                leading: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+                title: Image.asset(
+                  'assets/icon.png',
+                ),
+                actions: [Image.asset('assets/profile.png')],
               ),
-              Text('Tap to Ask!',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Nunito',
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold)),
-              Row(
-                children: [],
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              Container(
-                  height: 150,
-                  width: 150,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.mic_external_on_rounded),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: redColor,
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(24),
-                    ),
-                  ))
-            ])),
+              extendBodyBehindAppBar: true,
+              backgroundColor: backgroundColor,
+              body: SafeArea(
+                child: Center(
+                    child: Column(children: [
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text('Tap to Ask!',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Nunito',
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold)),
+                  Row(
+                    children: [],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Container(
+                      height: 150,
+                      width: 150,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Image.asset('assets/mic.png'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: redColor,
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(24),
+                        ),
+                      )),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/note.png'),
+                      SizedBox(width: 20.0),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        height: 100,
+                        width: 185,
+                        child: Text('We help get your things done !',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Nunito',
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('Integrate with your email',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Nunito',
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                          color: grayColor,
+                          borderRadius: BorderRadius.circular(12.0)),
+                      padding: EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Expanded(
+                              child: TextField(
+                            autocorrect: true,
+                            style: const TextStyle(color: redColor),
+                            controller: _textController,
+                            decoration: const InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: 'Your mail',
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                            ),
+                          )),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Container(
+                              height: 40,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  color: redColor,
+                                  borderRadius: BorderRadius.circular(12.0)),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Submit',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Nunito',
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold)),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: redColor,
+                                  padding: EdgeInsets.all(3.0),
+                                ),
+                              )),
+                        ],
+                      ))
+                ])),
 
-            /*
+                /*
                 Row(
                   children: [
                     Icon(Icons.pause_presentation),
@@ -94,8 +174,8 @@ class _HomePageState extends State<HomePage> {
                   ),
                 )
                 */
-          ),
-        ));
+              ),
+            )));
   }
 }
 
